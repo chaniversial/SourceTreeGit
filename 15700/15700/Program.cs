94a921a03fecf0c30000aca12d1791ef4d6cb155
chaniversial <chanho8361@naver.com>
2022-11-27 19:56:49 +0900

    https://www.acmicpc.net/problem/15700

---
+++ 15700/15700/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] info = Console.ReadLine().Split();
+ulong n = ulong.Parse(info[0]);
+ulong m = ulong.Parse(info[1]);
+ulong size = n * m;
+if (size % 2 != 0)
+    size--;
+    
+Console.WriteLine(size/2);
