18433b315d84b77d549ce09898c79a0698afe026
chaniversial <chanho8361@naver.com>
2022-11-27 20:15:45 +0900

    https://www.acmicpc.net/workbook/view/2032

---
+++ 15963/15963/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] info = Console.ReadLine().Split();
+ulong n = ulong.Parse(info[0]);
+ulong m = ulong.Parse(info[1]);
+
+if (n == m)
+    Console.WriteLine(1);
+else
+    Console.WriteLine(0);
